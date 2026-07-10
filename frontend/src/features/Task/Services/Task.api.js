4022a6f6c96714fc78d262f6a81e9050707d25d5
RohitMahajan8007 <rohitmahajan800737@gmail.com>
2026-07-10 12:25:20 +0530

    changes

--- frontend/src/features/Task/Services/Task.api.js
+++ frontend/src/features/Task/Services/Task.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const taskApi = axios.create({
-    baseURL: "/api/tasks",
+    baseURL: `${import.meta.env.VITE_API_URL || ""}/api/tasks`,
     withCredentials: true,
 });
 
